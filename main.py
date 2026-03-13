e588f8260ad1257fe6d6ece1118e0c34402de271
Rakshit Verma <r.verma@ubiquedigital.com>
2026-03-13 10:16:39 +0530

    First attempt

---
+++ main.py
@@ -0,0 +1,50 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import joblib
+import os
+
+app = FastAPI()
+
+MODEL_VERSION = "1.0"
+
+# Load model once when API starts
+model_path = os.path.join("model", "iris_model.pkl")
+model = joblib.load(model_path)
+
+# Class names from Iris dataset
+class_names = ["setosa", "versicolor", "virginica"]
+
+
+class FlowerInput(BaseModel):
+    sepal_length: float
+    sepal_width: float
+    petal_length: float
+    petal_width: float
+
+
+@app.get("/")
+def home():
+    return {"message": "Iris ML API is running 🚀"}
+
+
+@app.post("/predict")
+def predict(data: FlowerInput):
+
+    input_data = [[
+        data.sepal_length,
+        data.sepal_width,
+        data.petal_length,
+        data.petal_width
+    ]]
+
+    prediction_index = model.predict(input_data)[0]
+    prediction_label = class_names[prediction_index]
+
+    probabilities = model.predict_proba(input_data)[0]
+    confidence = max(probabilities)
+
+    return {
+        "prediction": prediction_label,
+        "confidence": float(confidence),
+        "model_version": MODEL_VERSION
+    }
---
+++ train.py
@@ -0,0 +1,55 @@
+import os
+import joblib
+import pandas as pd
+
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import accuracy_score, confusion_matrix
+
+
+def train_model():
+
+    # Load dataset
+    iris = load_iris()
+    X = pd.DataFrame(iris.data, columns=iris.feature_names)
+    y = iris.target
+
+    # Train-test split
+    X_train, X_test, y_train, y_test = train_test_split(
+        X,
+        y,
+        test_size=0.2,
+        random_state=42
+    )
+
+    # Create pipeline
+    pipeline = Pipeline([
+        ('scaler', StandardScaler()),
+        ('model', LogisticRegression(max_iter=200))
+    ])
+
+    # Train
+    pipeline.fit(X_train, y_train)
+
+    # Evaluate
+    y_pred = pipeline.predict(X_test)
+    accuracy = accuracy_score(y_test, y_pred)
+    cm = confusion_matrix(y_test, y_pred)
+
+    print("Model Accuracy:", accuracy)
+    print("Confusion Matrix:\n", cm)
+
+    # Create model folder if not exists
+    os.makedirs("model", exist_ok=True)
+
+    # Save model
+    joblib.dump(pipeline, "model/iris_model.pkl")
+
+    print("Model saved successfully in model/iris_model.pkl")
+
+
+if __name__ == "__main__":
+    train_model()
